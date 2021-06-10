d4fe2a81501d50c372ff0bdc921dd1054c1bd0f7
unknown <liuhaiwei2016@gmail.com>
2021-06-10 16:05:15 +0200

    HOC

---
+++ 4.hoc/src/App.js
@@ -0,0 +1,54 @@
+/** @format */
+
+import "./App.css";
+import React from "react";
+
+function withMouse(WrappedComponent) {
+  class Mouse extends React.Component {
+    state = {
+      x: 0,
+      y: 0,
+    };
+    handleMouseMove = (e) => {
+      this.setState({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+
+    componentDidMount() {
+      window.addEventListener("mousemove", this.handleMouseMove);
+    }
+
+    componentWillUnmount() {
+      window.removeEventListener("mousemove", this.handleMouseMove);
+    }
+    render() {
+      return <WrappedComponent {...this.state} />;
+    }
+  }
+
+  return Mouse;
+}
+
+const Position = (props) => (
+  <p>
+    Mouse current position: (x:{props.x} y:{props.y})
+  </p>
+);
+
+const MousePosition = withMouse(Position);
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>High order component</h1>
+
+        <MousePosition></MousePosition>
+      </div>
+    );
+  }
+}
+
+export default App;
